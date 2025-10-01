21332acb5bc4c1771c39ace4ce4ad666738aa76a
borditamas <borditamas@gmail.com>
2025-10-01 15:49:15 +0200

    client_device (not finished)

--- client_device/python/utils/oid.py
+++ client_device/python/utils/oid.py
@@ -2,8 +2,8 @@ from enum import Enum
 from typing import List
 
 class OID(Enum):
-    CPU_TOTAL_LOAD = "1_4"
-    RAM_FREE = "2_1"
+    CPU_TOTAL_LOAD = "1.4"
+    RAM_FREE = "2.1"
 
     @classmethod
     def values(cls) -> List[str]:
